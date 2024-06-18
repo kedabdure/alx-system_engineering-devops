34c68321a252927e403ad36eb2914efffc4fa01e
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-06-18 15:07:03 +0300

    Update 100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,16 +3,18 @@
 '''
 import requests
 
+
 def print_sorted_histogram(histogram):
     '''Sorts and prints the histogram in descending order of word frequency.
     '''
-    filtered_histogram = {k: v for k, v in histogram if v > 0}
+    filtered_histogram = {k: v for k, v in histogram.items() if v > 0}
     sorted_histogram = sorted(
         filtered_histogram.items(),
         key=lambda kv: (-kv[1], kv[0])
     )
     for word, count in sorted_histogram:
         print(f'{word}: {count}')
+
 
 def count_words(subreddit, word_list, histogram=None, count=0, after=None):
     '''Counts occurrences of each word in the given word list within a subreddit's titles.
@@ -28,17 +30,18 @@ def count_words(subreddit, word_list, histogram=None, count=0, after=None):
                       'Chrome/97.0.4692.71 Safari/537.36 '
                       'Edg/97.0.1072.62'
     }
-    
+
     params = {
         'sort': 'hot',
         'limit': 30,
         'count': count,
         'after': after
     }
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/.json'
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
     if response.status_code == 200:
         data = response.json().get('data', {})
         posts = data.get('children', [])
@@ -50,7 +53,8 @@ def count_words(subreddit, word_list, histogram=None, count=0, after=None):
                 histogram[word] += words.count(word)
 
         if len(posts) == params['limit'] and data.get('after'):
-            count_words(subreddit, word_list, histogram, count + len(posts), data['after'])
+            count_words(subreddit, word_list, histogram,
+                        count + len(posts), data['after'])
         else:
             print_sorted_histogram(histogram)
     else:

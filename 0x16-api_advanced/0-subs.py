570949c0d0a0b136eff40efd704b3c21427ae1bc
Abdurehim <128035951+kedabdure@users.noreply.github.com>
2024-06-18 14:56:16 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API and returns the number of subscribers for a given subreddit. """
     headers = {
-        'User-Agent': 'Mozilla/5.0'
+        'User-Agent': 'Mozilla/5.0 (compatible; MyRedditApp/1.0; +https://www.example.com/my-reddit-app)'
     }
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
@@ -20,3 +20,10 @@ def number_of_subscribers(subreddit):
 
     data = response.json().get('data', {})
     return data.get('subscribers', 0)
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
